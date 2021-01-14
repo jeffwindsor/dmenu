8cf32c5c12abfe8f15ce63c299ac6505855b24f1
Jeff Windsor <jeff.windsor@gmail.com>
2021-01-13 21:19:14 -0800

    upgrade center patch

--- config.h
+++ config.h
@@ -1,8 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -b  option; if 0, dmenu appears at bottom     */
+static int topbar                      = 1;
+/* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy                       = 1;  
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines              = 0;
+/* -c option; centers dmenu on screen */
+static int centered                    = 0;
+/* minimum width when centered */
+static int min_width                   = 500;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight         = 0;
+static unsigned int min_lineheight     = 8;
+/* Size of the window border */
+static const unsigned int border_width = 5;
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -10,23 +24,14 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNorm]          = { "#bbbbbb", "#222222" },
+	[SchemeSel]           = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight]  = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
-
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static const unsigned int border_width = 5;
